42ab9fd5627062eda6c18ae60e055b57a83730c7
Forgay <33027323+Forgay@users.noreply.github.com>
2018-06-20 14:13:29 +0200

    Update mailer.php

--- Config/mailer.php
+++ Config/mailer.php
@@ -1,11 +1,11 @@
 
 <<<<<<< HEAD
 return [
-  "host" => 'smtp-relay.sendinblue.com',
-  "port" => 587,
-    "encryption" =>'ssl',
-    "username" =>'gthareau1@gmail.com',
-    "password" =>'tqShMLXx57Amy2OP'
+  "host" => '',
+  "port" => ,
+    "encryption" =>'',
+    "username" =>'',
+    "password" =>''
 ];
 =======
 >>>>>>> master
