e24e1fa1b0238b1243c1921503099382dc0e9a99
Forgay <gthareau1@gmail.com>
2018-07-04 17:00:59 +0200

    addmentions

--- Config/mailer.php
+++ Config/mailer.php
@@ -1,5 +1,4 @@
-
-<<<<<<< HEAD
+<?php
 return [
   "host" => 'smtp-relay.sendinblue.com',
   "port" => 587,
@@ -7,5 +6,4 @@ return [
     "username" =>'gthareau1@gmail.com',
     "password" =>'tqShMLXx57Amy2OP'
 ];
-=======
->>>>>>> master
+
